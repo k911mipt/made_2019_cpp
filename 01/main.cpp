a7edcf50a7063147dbe8d7830e12f0005dc4cf56
Dmitrii Rogoznyi <k911mipt@gmail.com>
2019-10-14 15:06:28 -0700

    fix output stream for usage info

--- 01/main.cpp
+++ 01/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char** argv)
 {
     if (argc <= 1) {
-        std::cerr << "Usage: " << argv[0] << " <expression>" << std::endl;
+        std::cout << "Usage: " << argv[0] << " <expression>" << std::endl;
         return EXIT_SUCCESS;
     }
     const char* expression = argv[1];
